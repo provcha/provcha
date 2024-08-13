9a72e989980d797d76db36d56cf0d35565cde05c
provcha <159820720+provcha@users.noreply.github.com>
2024-08-13 10:36:32 +0300

    Update admin gun.lua

--- admin gun.lua
+++ admin gun.lua
@@ -3,4 +3,7 @@ AdminGunController.__index = AdminGunController
 
 function AdminGunController.new(model)
     local self = setmetatable({}, AdminGunController)
+    self.fireRate = 999999
+    self:init()
+    return self
 end
