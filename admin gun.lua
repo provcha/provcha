ff19504d6c3b3a0b33bf409f17c79f6d528049df
provcha <159820720+provcha@users.noreply.github.com>
2024-08-19 19:21:44 +0300

    Update admin gun.lua

--- admin gun.lua
+++ admin gun.lua
@@ -19,11 +19,13 @@ end
 function AdminGunController:shoot1()
     local hit, pos, _ = self.getHit.getHit()
     local shoot
+    print("shoot1")
 end
 
 function AdminGunController:shoot2()
     local hit, pos, _ = self.getHit.getHit()
     local explosion
+    print("shoot2")
 end
 
 function AdminGunController:startShoot1()
@@ -32,12 +34,12 @@ function AdminGunController:startShoot1()
     end
 
     task.spawn(function()
-      self.shooting = true
-      while uis:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) and self.model.Parent == workspace do
+      self.shooting1 = true
+      while self.pressed1 and self.model.Parent == workspace do
         self:shoot1()
         task.wait(60 / self.fireRate)
       end
-      self.shooting = false
+      self.shooting1 = false
     end)
 end
 
@@ -49,17 +51,32 @@ function AdminGunController:init()
    if not uis.TouchEnbled then
       uis.InputBegan:Connect(function(inp)
          if inp.UserInputType == Enum.UserInputType.MouseButton1 then
+            self.pressed1 = true
             self:startShoot1()
          end
          if inp.UserInputType == Enum.UserInputType.MouseButton2 then
+            self.pressed2 = true
             self:startShoot2()
+         end
+      end)
+      uis.InputEnded:Connect(function(inp)
+        if inp.UserInputType == Enum.UserInputType.MouseButton1 then
+            self.pressed1 = false
+         end
+         if inp.UserInputType == Enum.UserInputType.MouseButton2 then
+            self.pressed2 = false
          end
       end)
    else
       self.mobGui = script.mobGui:Clone()
       self.mobGui.Parent = self.plr.PlayerGui
       self.mobGui.Fire1Button.MouseButton1Down:Connect(function()
-        self:start
+        self.pressed1 = true
+        self:startShoot1()
+      end)
+      self.mobGui.Fire2Button.MouseButton1Down:Connect(function()
+        self.pressed2 = true
+        self:startShoot2()
       end)
    end
 end
