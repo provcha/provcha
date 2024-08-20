6bf1944caf927a9d93e888bf0a3be3cde868faeb
provcha <159820720+provcha@users.noreply.github.com>
2024-08-20 12:02:27 +0300

    Update admin gun.lua

--- admin gun.lua
+++ admin gun.lua
@@ -3,44 +3,44 @@ AdminGunController.__index = AdminGunController
 local uis = game:GetService("UserInputService")
 
 function AdminGunController.new(model)
-    local self = setmetatable({}, AdminGunController)
-    self.plr = game.Players.LocalPlayer
-    self.model = model
-    self.fireRate = 999999
-    self.getHit = require(game.ReplicatedStorage.getHit)
-    self.shooting1 = false
-    self.shooting2 = false
-    self.perssed1 = false
-    self.pressed2 = false
-    self:init()
-    return self
+	local self = setmetatable({}, AdminGunController)
+	self.plr = game.Players.LocalPlayer
+	self.model = model
+	self.fireRate = 999999
+	self.getHit = require(game.ReplicatedStorage.getHit)
+	self.shooting1 = false
+	self.shooting2 = false
+	self.perssed1 = false
+	self.pressed2 = false
+	self:init()
+	return self
 end
 
 function AdminGunController:shoot1()
-    local hit, pos, _ = self.getHit.getHit()
-    local shoot
-    print("shoot1")
+	local hit, pos, _ = self.getHit.getHit()
+	local shoot
+	print("shoot1")
 end
 
 function AdminGunController:shoot2()
-    local hit, pos, _ = self.getHit.getHit()
-    local explosion
-    print("shoot2")
+	local hit, pos, _ = self.getHit.getHit()
+	local explosion
+	print("shoot2")
 end
 
 function AdminGunController:startShoot1()
-    if self.shooting then
-       return
-    end
+if self.shooting then
+return
+end
 
-    task.spawn(function()
-      self.shooting1 = true
-      while self.pressed1 and self.model.Parent == workspace do
-        self:shoot1()
-        task.wait(60 / self.fireRate)
-      end
-      self.shooting1 = false
-    end)
+task.spawn(function()
+self.shooting1 = true
+while self.pressed1 and self.model.Parent == workspace do
+self:shoot1()
+task.wait(60 / self.fireRate)
+end
+self.shooting1 = false
+end)
 end
 
 function AdminGunController:startShoot2()
