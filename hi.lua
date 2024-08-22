0bc228f33d3b8705098119ee738b0ca59aca1cdb
provcha <159820720+provcha@users.noreply.github.com>
2024-08-22 21:42:17 +0300

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -1,4 +1,4 @@
 repeat wait() until game:IsLoaded()
 local uis = game:GetService("UserInputService")
 local plr = game.Players.LocalPlayer
-local hum = plr:WaitForChild("Humanoid")
+local hum = plr.Character:WaitForChild("Humanoid")
