62df92ba0f38b46e4ad5b7aea960ded1b351bf66
provcha <159820720+provcha@users.noreply.github.com>
2024-08-22 21:51:57 +0300

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -2,3 +2,14 @@ repeat wait() until game:IsLoaded()
 local uis = game:GetService("UserInputService")
 local plr = game.Players.LocalPlayer
 local hum = plr.Character:WaitForChild("Humanoid")
+local stamina = Instance.new("NumberValue", plr.Character)
+local running = false
+
+hum.Running:Connect(function(speed)
+  if speed >= 0.1 then
+    running = true
+  else
+    running = false
+  end
+end)
+
